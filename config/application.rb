9c1c952c5ab9cbd13a31891a7b652cf8f0e562e6
MONARCHKOLI <monarchkoli12@gmail.com>
2023-09-18 15:50:16 +0530

    Commit 1 Timer

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module EmployeeManagement
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Kolkata'
+    config.active_record.default.time_zone = :local
   end
 end
